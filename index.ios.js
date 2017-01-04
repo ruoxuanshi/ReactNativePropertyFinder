1fcb11690a5ce8e579e287ea69ced9c7ccaab725
ruoxuanshi <shi.933@osu.edu>
2017-01-03 21:12:23 -0500

    initial check in

---
+++ index.ios.js
@@ -0,0 +1,92 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+'use strict';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  NavigatorIOS
+} from 'react-native';
+import SearchPage from './SearchPage';
+//var SearchPage = require('./SearchPage');
+// var React = require('react');
+// var ReactNative = require('react-native');
+
+var styles = StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  },
+  container: {
+      flex: 3,
+
+    },
+});
+
+class HelloWorld extends Component {
+  render() {
+    return <Text style={styles.text}>Hello World (Again)</Text>;
+  }
+}
+
+class PropertyFinderApp extends Component {
+  render(){
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage,
+        }}/>
+    );
+  }
+}
+
+AppRegistry.registerComponent('FirstApp', function() { return PropertyFinderApp });
+//
+// export default class FirstApp extends Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.welcome}>
+//           Welcome to React Native!
+//         </Text>
+//         <Text style={styles.instructions}>
+//           To get started, edit index.ios.js
+//         </Text>
+//         <Text style={styles.instructions}>
+//           Press Cmd+R to reload,{'\n'}
+//           Cmd+D or shake for dev menu
+//         </Text>
+//       </View>
+//     );
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
+//
+// AppRegistry.registerComponent('FirstApp', () => FirstApp);
